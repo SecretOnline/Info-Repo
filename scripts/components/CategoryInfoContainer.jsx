88fb2531f7996fef0fed2544d94b75f6a2158ed8
secret_online <me+github@secretonline.co>
2016-06-17 09:54:51 +1200

    Filter the list by category

--- scripts/components/CategoryInfoContainer.jsx
+++ scripts/components/CategoryInfoContainer.jsx
@@ -26,6 +26,35 @@ window.repo.CategoryInfoContainer = React.createClass({
     }
   },
   render: function() {
+    var self = this;
+    if (this.state.category) {
+      var filteredList = this.props.info.filter(function(info) {
+        return info.categories.find(function(category) {
+          return category.title === self.state.category.title;
+        });
+      });
+
+      if (filteredList.length) {
+        return (
+          <div>
+            <h2>Categories</h2>
+            <repo.CategoryCardList categories={this.props.categories} click={this.categoryClick} />
+            <h2>Information</h2>
+            <repo.InfoCardList categories={this.props.categories} info={filteredList} click={this.infoClick} />
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <h2>Categories</h2>
+            <repo.CategoryCardList categories={this.props.categories} click={this.categoryClick} />
+            <h2>Information</h2>
+            <p>No results were found for this category</p>
+          </div>
+        );
+      }
+    }
+
     return (
       <div>
         <h2>Categories</h2>
