7fd65505dc6488faeda36d4d5264027827284ef5
secret_online <me+github@secretonline.co>
2016-06-16 18:33:01 +1200

    Add click callback property for cards

--- scripts/components/CategoryCard.jsx
+++ scripts/components/CategoryCard.jsx
@@ -5,8 +5,12 @@ window.repo.CategoryCard = React.createClass({
       data: {
         title: 'Loading...',
         class: 'default'
-      }
-    }
+      },
+      click: null
+    };
+  },
+  doClickCallback() {
+    this.props.click(this.props.data);
   },
   render: function() {
     // Create the main
@@ -16,12 +20,22 @@ window.repo.CategoryCard = React.createClass({
       'cat-' + this.props.data.class
     ];
 
-    return (
-      <div className={classes.join(' ')} id={window.repo.modTitle(this.props.data.title)}>
-        <div className="header">
-          <h3 className="card-title" key="header-title">{this.props.data.title}</h3>
+    if (this.props.click && typeof this.props.click === 'function') {
+      return (
+        <div className={classes.join(' ')} id={window.repo.modTitle(this.props.data.title)}>
+          <div className="header" onClick={this.doClickCallback}>
+            <h3 className="card-title" key="header-title">{this.props.data.title}</h3>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className={classes.join(' ')} id={window.repo.modTitle(this.props.data.title)}>
+          <div className="header">
+            <h3 className="card-title" key="header-title">{this.props.data.title}</h3>
+          </div>
+        </div>
+      );
+    }
   }
 });
