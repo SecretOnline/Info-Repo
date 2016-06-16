308c1208544b11970d23b1c97dc3e089bb4c113d
secret_online <me+github@secretonline.co>
2016-06-16 19:31:42 +1200

    Duplicate InfoCard components

---
+++ scripts/components/ElementCardList.jsx
@@ -0,0 +1,43 @@
+window.repo = window.repo || {};
+window.repo.InfoCardList = React.createClass({
+  getDefaultProps: function() {
+    return {
+      info: [],
+      categories: [],
+      click: null
+    };
+  },
+  render: function() {
+    var self = this;
+    var cardNodes = this.props.info.sort(function(a, b) {
+      if (a.title < b.title)
+        return -1;
+      if (a.title > b.title)
+        return 1;
+      return 0;
+    }).map(function(info) {
+      // Replace category names with category objects
+      info.categories.forEach(function(cat, index) {
+        info.categories[index] = self.props.categories.find(function(item) {
+          return item.title === cat;
+        }) || info.categories[index];
+      });
+
+      if (self.props.click && typeof self.props.click === 'function') {
+        return (
+          <repo.InfoCard click={self.props.click} data={info} key={info.title} />
+        );
+      } else {
+        return (
+          <repo.InfoCard data={info} key={info.title} />
+        );
+      }
+    });
+
+    return (
+      <div className="card-list info-card-list">
+        {cardNodes}
+      </div>
+    );
+  }
+});
