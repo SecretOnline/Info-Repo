04ef01692e52fc51867783aaa801fabecdf836c3
secret_online <me+github@secretonline.co>
2016-06-16 18:16:22 +1200

    Rewrite CardLists to use properties rather than state

--- scripts/components/CategoryCardList.jsx
+++ scripts/components/CategoryCardList.jsx
@@ -1,31 +1,24 @@
 window.repo = window.repo || {};
 window.repo.CategoryCardList = React.createClass({
-  getInitialState: function() {
+  getDefaultProps: function() {
     return {
-      data: []
-    };
-  },
-  componentDidMount: function() {
-    var self = this;
-    var categoryPromise = window.repo.get(this.props.caturl)
-      .then(JSON.parse);
-    categoryPromise
-      .then(function(categories) {
-        self.setState({data: categories});
-      });
+      categories: []
+    }
   },
   render: function() {
-    var cardNodes = this.state.data.sort(function(a, b) {
-      if (a.title < b.title)
-        return -1;
-      if (a.title > b.title)
-        return 1;
-      return 0;
-    }).map(function(cardData) {
-      return (
-        <repo.CategoryCard data={cardData} key={cardData.title} />
-      )
-    });
+    var cardNodes = this.props.categories
+      .sort(function(a, b) {
+        if (a.title < b.title)
+          return -1;
+        if (a.title > b.title)
+          return 1;
+        return 0;
+      })
+      .map(function(cardData) {
+        return (
+          <repo.CategoryCard data={cardData} key={cardData.title} />
+        )
+      });
 
     return (
       <div className="card-list category-card-list">
