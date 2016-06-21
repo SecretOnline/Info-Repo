043bc782ad10a633b4527ca3143afbda2015afa9
secret_online <me+github@secretonline.co>
2016-06-22 10:37:37 +1200

    Make info section work
    Not particularly clean, but it works.

--- scripts/components/InfoPage.jsx
+++ scripts/components/InfoPage.jsx
@@ -1,10 +1,5 @@
 window.repo = window.repo || {};
 window.repo.InfoPage = React.createClass({
-  getInitialState: function() {
-    return {
-      spotlight: null
-    };
-  },
   getDefaultProps: function() {
     return {
       info: []
@@ -17,23 +12,41 @@ window.repo.InfoPage = React.createClass({
     this.setState({spotlight: null});
   },
   render: function() {
+    var self = this;
 
-    if (this.state.spotlight) {
+    if (this.props.routeParams.info) {
+      console.log(this.props.routeParams.info);
+
+      var spotlight = this.props.route.info.find(function(info) {
+        return self.props.routeParams.info === repo.modTitle(info.title);
+      });
+
+      if (!spotlight) {
+        spotlight = {
+          title: 'Info Not Found',
+          text: [
+            'Unfortunately, we weren\'t able to find any information on what you were looking for',
+            'If you came from outside the Information Repository, please check the URL and make sure it\'s valid'
+          ],
+          spoiler: true
+        }
+      }
+
       return (
         <div class="page info-page">
-          <repo.InfoSpotlight data={this.state.spotlight} click={this.removeSpotlight} />
+          <repo.InfoSpotlight data={spotlight} link="/info" />
           <h2>Information</h2>
-          <repo.InfoCardList info={this.props.route.info} click={this.setSpotlight} />
-        </div>
-      );
-    } else {
-      return (
-        <div class="page info-page">
-          <h2>Information</h2>
-          <repo.InfoCardList info={this.props.route.info} click={this.setSpotlight} />
+          <repo.InfoCardList info={this.props.route.info} link="/info" />
         </div>
       );
     }
+
+    return (
+      <div class="page info-page">
+        <h2>Information</h2>
+        <repo.InfoCardList info={this.props.route.info} click={this.setSpotlight} />
+      </div>
+    );
 
   }
 });
--- scripts/components/InfoSpotlight.jsx
+++ scripts/components/InfoSpotlight.jsx
@@ -23,18 +23,22 @@ window.repo.InfoSpotlight = React.createClass({
       'spotlight',
       'info-card'
     ];
-    if (this.props.data.categories.length) {
-      classes.push('cat-' + this.props.data.categories[0].class);
+
+    if (this.props.data.spoiler) {
+      classes.push('spoiler');
     }
 
     // Set header stuff
     var headerList = [
       (<h3 className="card-title" key="header-title">{this.props.data.title}</h3>)
     ];
-    var categoryNodes = this.props.data.categories.map(function(cat) {
-      return (<li data-id={cat.class} key={cat.class}><a href={'cat#' + window.repo.modTitle(cat.title)}>{cat.title}</a></li>)
-    });
-    headerList.push((<ul className="categories" key="header-categories">{categoryNodes}</ul>));
+    if (this.props.data.categories && this.props.data.categories.length) {
+      classes.push('cat-' + this.props.data.categories[0].class);
+      var categoryNodes = this.props.data.categories.map(function(cat) {
+        return (<li data-id={cat.class} key={cat.class}><ReactRouter.Link to={'/categories/' + window.repo.modTitle(cat.title)}>{cat.title}</ReactRouter.Link></li>)
+      });
+      headerList.push((<ul className="categories" key="header-categories">{categoryNodes}</ul>));
+    }
     // Set card content stuff
     var bodyList = [];
     if (this.props.data.text && this.props.data.text.length) {
@@ -57,22 +61,37 @@ window.repo.InfoSpotlight = React.createClass({
     if (this.props.data.related && this.props.data.related.length) {
       var relatedNodes = this.props.data.related.map(function(rel) {
         return (
-          <li key={rel}><a href={'#' + window.repo.modTitle(rel)}>{rel}</a></li>
+          <li key={rel}><ReactRouter.Link to={self.props.link + '/' + repo.modTitle(info.title)}>{rel}</ReactRouter.Link></li>
         )
       });
       bodyList.push((<h4 key="body-related-title">Related</h4>));
       bodyList.push((<ul className="related" key="body-related">{relatedNodes}</ul>));
     }
 
-    return (
-      <div className={classes.join(' ')} id={window.repo.modTitle(this.props.data.title)}>
-        <div className="header" onClick={this.doClickCallback}>
-          {headerList}
+    if (this.props.link) {
+      return (
+        <div className={classes.join(' ')} id={window.repo.modTitle(this.props.data.title)}>
+          <div className="header" onClick={this.doClickCallback}>
+            {headerList}
+          </div>
+          <div className="card-content">
+            {bodyList}
+          </div>
         </div>
-        <div className="card-content">
-          {bodyList}
+      );
+    } else {
+      return (
+        <div className={classes.join(' ')} id={window.repo.modTitle(this.props.data.title)}>
+          <div className="header" onClick={this.doClickCallback}>
+            {headerList}
+          </div>
+          <div className="card-content">
+            {bodyList}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+
+
   }
 });
