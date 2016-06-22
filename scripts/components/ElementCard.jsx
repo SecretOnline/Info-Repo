dd8f3a20ef21c8b4fabedd142a3e964217061bdc
secret_online <me+github@secretonline.co>
2016-06-22 14:56:09 +1200

    Add individual element routing

--- scripts/components/ElementCard.jsx
+++ scripts/components/ElementCard.jsx
@@ -1,10 +1,5 @@
 window.repo = window.repo || {};
 window.repo.ElementCard = React.createClass({
-  getInitialState: function() {
-    return {
-      expanded: false
-    };
-  },
   getDefaultProps: function() {
     return {
       data: {
@@ -18,25 +13,12 @@ window.repo.ElementCard = React.createClass({
       click: null
     };
   },
-  toggleExpanded: function() {
-    this.setState({expanded: !this.state.expanded});
-  },
-  doClickCallback() {
-    if (this.props.click && typeof this.props.click === 'function') {
-      this.props.click(this.props.data);
-    } else {
-      this.toggleExpanded();
-    }
-  },
   render: function() {
     // Create the main
     var classes = [
       'card',
       'element-card'
     ];
-    if (this.state.expanded) {
-      classes.push('expanded');
-    }
 
     var headerStyle = {};
     if (this.props.data.color) {
@@ -50,45 +32,14 @@ window.repo.ElementCard = React.createClass({
     ];
 
 
-    if (!this.state.expanded) {
-      return (
-        <div className={classes.join(' ')} id={window.repo.modTitle(this.props.data.title)}>
-          <div className="header" onClick={this.doClickCallback} style={headerStyle}>
+    return (
+      <div className={classes.join(' ')} id={window.repo.modTitle(this.props.data.title)}>
+        <ReactRouter.Link to={this.props.link}>
+          <div className="header">
             {headerList}
           </div>
-        </div>
-      );
-    } else {
-      // Set card content stuff
-      var bodyList = [];
-      if (this.props.data.text && this.props.data.text.length) {
-        var descNodes = this.props.data.text.map(function(desc, index) {
-          return (
-            <p key={index}>{desc}</p>
-          )
-        });
-        bodyList.push((<div className="information" key="body-desc">{descNodes}</div>));
-      }
-      if (this.props.data.places && this.props.data.places.length) {
-        var placeNodes = this.props.data.places.map(function(place) {
-          return (
-            <li key={place}>{place}</li>
-          )
-        });
-        bodyList.push((<h4 key="body-places-title">Places</h4>));
-        bodyList.push((<ul className="places" key="body-places">{placeNodes}</ul>));
-      }
-
-      return (
-        <div className={classes.join(' ')} id={window.repo.modTitle(this.props.data.title)}>
-          <div className="header" onClick={this.doClickCallback} style={headerStyle}>
-            {headerList}
-          </div>
-          <div className="card-content">
-            {bodyList}
-          </div>
-        </div>
-      );
-    }
+        </ReactRouter.Link>
+      </div>
+    );
   }
 });
--- scripts/components/ElementPage.jsx
+++ scripts/components/ElementPage.jsx
@@ -17,23 +17,39 @@ window.repo.ElementPage = React.createClass({
     this.setState({spotlight: null});
   },
   render: function() {
+    var self = this;
 
-    if (this.state.spotlight) {
+    if (this.props.routeParams.element) {
+
+      var spotlight = this.props.route.elements.find(function(info) {
+        return self.props.routeParams.element === repo.modTitle(info.title);
+      });
+
+      if (!spotlight) {
+        spotlight = {
+          title: 'Info Not Found',
+          text: [
+            'Unfortunately, we weren\'t able to find any information on what you were looking for',
+            'If you came from outside the Information Repository, please check the URL and make sure it\'s valid'
+          ],
+          spoiler: true
+        }
+      }
+
       return (
         <div class="page element-page">
-          <repo.ElementSpotlight data={this.state.spotlight} click={this.removeSpotlight} />
+          <repo.ElementSpotlight data={spotlight} link="/elements" />
           <h2>Elements</h2>
-          <repo.ElementCardList elements={this.props.route.elements} click={this.setSpotlight} />
-        </div>
-      );
-    } else {
-      return (
-        <div class="page element-page">
-          <h2>Elements</h2>
-          <repo.ElementCardList elements={this.props.route.elements} click={this.setSpotlight} />
+          <repo.ElementCardList elements={this.props.route.elements} link="/elements" />
         </div>
       );
     }
 
+    return (
+      <div class="page element-page">
+        <h2>Elements</h2>
+        <repo.ElementCardList elements={this.props.route.elements} link="/elements" />
+      </div>
+    );
   }
 });
