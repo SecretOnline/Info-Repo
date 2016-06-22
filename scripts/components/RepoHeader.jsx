cd7e5e99ec195cf100bb808cebec87a333cc7be2
secret_online <me+github@secretonline.co>
2016-06-22 16:56:29 +1200

    Add nav toggling functions

--- scripts/components/RepoHeader.jsx
+++ scripts/components/RepoHeader.jsx
@@ -1,11 +1,17 @@
 window.repo = window.repo || {};
 window.repo.RepoHeader = React.createClass({
+  toggleNav: function() {
+    var nav = document.querySelector('nav');
+    if (nav) {
+      nav.classList.toggle('open');
+    }
+  },
   render: function() {
     return (
       <header>
         <h1 className="title"><ReactRouter.Link to="/">NMS Information Repository</ReactRouter.Link></h1>
         <div className="button-container">
-          <button type="button" className="menu">
+          <button type="button" className="menu" onClick={this.toggleNav}>
             <img src="res/menu.svg" alt="Navigation" />
           </button>
         </div>
