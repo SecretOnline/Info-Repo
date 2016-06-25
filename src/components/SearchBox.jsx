4a9ec5223685db25148ee445e5bb61a88b97959b
secret_online <me+github@secretonline.co>
2016-06-25 14:54:59 +1200

    Add search box component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,37 @@
+import helper from '../helper.jsx';
+
+export default class SearchBox extends React.Component {
+  static get defaultProps() {
+    return {
+      callback: null;
+    };
+  }
+
+  static get initialState() {
+    return {
+      text: ''
+    };
+  }
+
+  inputChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  submitSearch() {
+    if (this.props.callback) {
+      callback(this.state.text)
+    }
+  }
+
+  render() {
+    return (
+      <div className="search-area">
+        <input type="search" placeholder="Search the Repository" className="info-search-box" value={this.state.text} autofocus >
+        <input type="button" value="Search" className="info-search-button" >
+      </div>
+    );
+
+  }
+}
