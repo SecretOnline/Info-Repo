3735c6fbfd12f9f1363fe13fb9707b30f8102a32
secret_online <me+github@secretonline.co>
2016-06-25 20:31:48 +1200

    Add redirection

--- www/scripts/redirect.js
+++ www/scripts/redirect.js
@@ -1,4 +1,21 @@
 (function() {
   'use strict';
-  
+
+  var url = window.location.toString();
+  var regex = /.*\?(\w+)(?:=([\w%]+))?/;
+
+  var res = url.match(regex);
+  if (res) {
+    console.log('old url detected, need to redirect');
+
+    var page = res[1];
+    var item = res[2];
+
+    // And awaaayy we go!
+    if (item) {
+      window.location = page + '/' + item;
+    } else {
+      window.location = page;
+    }
+  }
 }());
