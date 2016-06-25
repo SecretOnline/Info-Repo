6fad900d3a8a7270bb811c99f47b02cb03d6d261
secret_online <me+github@secretonline.co>
2016-06-26 11:16:38 +1200

    Fix bad classes on nav

--- src/components/RepoNav.jsx
+++ src/components/RepoNav.jsx
@@ -11,9 +11,9 @@ export default class RepoNav extends React.Component {
       <nav>
         <ul>
           <li className="tab-info" onClick={this.closeNav}><ReactRouter.Link to="/info">Information</ReactRouter.Link></li>
-          <li className="tab-info" onClick={this.closeNav}><ReactRouter.Link to="/categories">Categories</ReactRouter.Link></li>
-          <li className="tab-info" onClick={this.closeNav}><ReactRouter.Link to="/elements">Elements</ReactRouter.Link></li>
-          <li className="tab-info" onClick={this.closeNav}><ReactRouter.Link to="/links">Links</ReactRouter.Link></li>
+          <li className="tab-categories" onClick={this.closeNav}><ReactRouter.Link to="/categories">Categories</ReactRouter.Link></li>
+          <li className="tab-elements" onClick={this.closeNav}><ReactRouter.Link to="/elements">Elements</ReactRouter.Link></li>
+          <li className="tab-links" onClick={this.closeNav}><ReactRouter.Link to="/links">Links</ReactRouter.Link></li>
         </ul>
       </nav>
     );
