aa59b0f0ab3d05f658a90c954801456a1cf4593c
secret_online <me+github@secretonline.co>
2016-06-26 18:29:12 +1200

    Add quick links on main page

--- src/components/RepoHeader.jsx
+++ src/components/RepoHeader.jsx
@@ -6,10 +6,17 @@ export default class RepoHeader extends React.Component {
     }
   }
 
+  closeNav() {
+    var nav = document.querySelector('nav');
+    if (nav) {
+      nav.classList.remove('open');
+    }
+  }
+
   render() {
     return (
       <header>
-        <h1 className="title"><ReactRouter.Link to="/">NMS Information Repository</ReactRouter.Link></h1>
+        <h1 className="title" onClick={this.closeNav}><ReactRouter.Link to="/">NMS Information Repository</ReactRouter.Link></h1>
         <div className="button-container">
           <button type="button" className="menu" onClick={this.toggleNav}>
             <img src="/res/menu.svg" alt="Navigation" />
