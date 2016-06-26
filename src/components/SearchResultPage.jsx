31156c319a69401cefb5b5e45529f02cb4bac51b
secret_online <me+github@secretonline.co>
2016-06-26 12:42:18 +1200

    Move search to seperate file

--- src/components/SearchResultPage.jsx
+++ src/components/SearchResultPage.jsx
@@ -1,5 +1,6 @@
 import SearchBox from '../components/SearchBox.jsx';
 import InfoCardList from '../components/InfoCardList.jsx';
+import search from '../search.jsx';
 
 export default class SearchResultPage extends React.Component {
   static get defaultProps() {
@@ -13,92 +14,6 @@ export default class SearchResultPage extends React.Component {
       ReactRouter.browserHistory.push(`/search/${encodeURIComponent(result)}`);
     }
 
-    function generalSearch(query, info) {
-      var scores = {};
-      var infoIndexArr = Object.keys(info);
-
-      // Get the score for a aprticulat piece of information
-      infoIndexArr.forEach((index) => {
-        var item = info[index];
-        var score = getSearchScore(query, item);
-
-        // Don't add anything with a score of 0
-        if (score > 0) {
-          if (!scores[score])
-            scores[score] = [];
-
-          scores[score].push(item);
-        }
-      });
-
-      var cardArr = [];
-      var values = Object.keys(scores);
-      // Sort using integers, not strings
-      values.sort((one, two) => {
-        // Sort as integers, not strings
-        var a = parseInt(one);
-        var b = parseInt(two);
-        if (a < b)
-          return -1;
-        if (a > b)
-          return 1;
-        return 0;
-      });
-
-      // Add information backwards (so highest score is at top)
-      values.forEach((score) => {
-        var arr = [];
-        scores[score].forEach((item) => {
-          arr.push(item);
-        });
-        cardArr = arr.concat(cardArr);
-      });
-
-      return cardArr;
-    }
-
-    function getSearchScore(query, info) {
-      query = query.toLowerCase();
-      var score = 0;
-
-      var querySplit = query.split(/\W+/);
-      var lTitle = info.title.toLowerCase();
-
-      // Title contains
-      if (lTitle.indexOf(query) > -1)
-        score += 3;
-      // Title contains parts
-      querySplit.forEach((word) => {
-        if (lTitle.match(new RegExp('\\s' + word + '\\s')))
-          score += 5;
-        if (lTitle.indexOf(word) > -1)
-          score += 2;
-      });
-      info.text.forEach((text) => {
-        var lText = text.toLowerCase();
-        // Text contains
-        if (lText.indexOf(query) > -1)
-          score += 2;
-        // Text contains parts
-        querySplit.forEach((word) => {
-          if (lText.match(new RegExp('\\s' + word + '\\s')))
-            score += 1;
-          if (lText.indexOf(word) > -1)
-            score += 1;
-        });
-      });
-      // Keywords
-      if (info.keywords) {
-        querySplit.forEach((word) => {
-          if (info.keywords.indexOf(word) > -1) {
-            score += 4;
-          }
-        });
-      }
-
-      return score;
-    }
-
     var query = this.props.routeParams.search;
     var infoList = this.props.route.info;
 
@@ -106,7 +21,7 @@ export default class SearchResultPage extends React.Component {
       <div class="page search-page">
         <h2>{`Search - ${query}`}</h2>
         <SearchBox callback={goTo} text={query} />
-        <InfoCardList info={generalSearch(query, infoList)} />
+        <InfoCardList info={search.generalSearch(query, infoList)} link={`/search/${encodeURIComponent(query)}`} />
       </div>
     );
   }
---
+++ src/search.jsx
@@ -0,0 +1,90 @@
+function generalSearch(query, info) {
+  var scores = {};
+  var infoIndexArr = Object.keys(info);
+
+  // Get the score for a aprticulat piece of information
+  infoIndexArr.forEach((index) => {
+    var item = info[index];
+    var score = getSearchScore(query, item);
+
+    // Don't add anything with a score of 0
+    if (score > 0) {
+      if (!scores[score])
+        scores[score] = [];
+
+      scores[score].push(item);
+    }
+  });
+
+  var cardArr = [];
+  var values = Object.keys(scores);
+  // Sort using integers, not strings
+  values.sort((one, two) => {
+    // Sort as integers, not strings
+    var a = parseInt(one);
+    var b = parseInt(two);
+    if (a < b)
+      return -1;
+    if (a > b)
+      return 1;
+    return 0;
+  });
+
+  // Add information backwards (so highest score is at top)
+  values.forEach((score) => {
+    var arr = [];
+    scores[score].forEach((item) => {
+      arr.push(item);
+    });
+    cardArr = arr.concat(cardArr);
+  });
+
+  return cardArr;
+}
+
+function getSearchScore(query, info) {
+  query = query.toLowerCase();
+  var score = 0;
+
+  var querySplit = query.split(/\W+/);
+  var lTitle = info.title.toLowerCase();
+
+  // Title contains
+  if (lTitle.indexOf(query) > -1)
+    score += 3;
+  // Title contains parts
+  querySplit.forEach((word) => {
+    if (lTitle.match(new RegExp('\\s' + word + '\\s')))
+      score += 5;
+    if (lTitle.indexOf(word) > -1)
+      score += 2;
+  });
+  info.text.forEach((text) => {
+    var lText = text.toLowerCase();
+    // Text contains
+    if (lText.indexOf(query) > -1)
+      score += 2;
+    // Text contains parts
+    querySplit.forEach((word) => {
+      if (lText.match(new RegExp('\\s' + word + '\\s')))
+        score += 1;
+      if (lText.indexOf(word) > -1)
+        score += 1;
+    });
+  });
+  // Keywords
+  if (info.keywords) {
+    querySplit.forEach((word) => {
+      if (info.keywords.indexOf(word) > -1) {
+        score += 4;
+      }
+    });
+  }
+
+  return score;
+}
+
+
+export default {
+  generalSearch
+};
