46b5bfe369a7126575ea63b9c1db6ae1036678ef
secret_online <me+github@secretonline.co>
2016-06-28 14:52:23 +1200

    Move areas of main.jsx into own functions

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,101 @@ import SearchResultPage from './components/SearchResultPage.jsx';
 import SearchSpotlightPage from './components/SearchSpotlightPage.jsx';
 import helper from './helper.jsx';
 
+function replaceCategories(arrays) {
+  // Replace category names with category objects
+  arrays[1].forEach((info) => {
+    info.categories.forEach((cat, index) => {
+      info.categories[index] = arrays[0].find((item) => {
+        return item.title === cat;
+      }) || info.categories[index];
+    });
+  });
+
+  return arrays;
+}
+
+function createRouterConfig(results) {
+  return {
+    path: '/',
+    component: RepoApp,
+    indexRoute: {
+      component: RepoHome
+    },
+    childRoutes: [
+      {
+        path: '/info',
+        component: InfoPage,
+        info: results[1]
+      },
+      {
+        path: '/info/:info',
+        component: InfoSpotlightPage,
+        info: results[1]
+      },
+      {
+        path: '/categories',
+        component: CategoryPage,
+        info: results[1],
+        categories: results[0]
+      },
+      {
+        path: '/categories/:category',
+        component: CategoryListPage,
+        info: results[1],
+        categories: results[0]
+      },
+      {
+        path: '/categories/:category/:info',
+        component: CategorySpotlightPage,
+        info: results[1],
+        categories: results[0]
+      },
+      {
+        path: '/elements',
+        component: ElementPage,
+        elements: results[2]
+      },
+      {
+        path: '/elements/:element',
+        component: ElementSpotlightPage,
+        elements: results[2]
+      },
+      {
+        path: '/links',
+        component: LinkPage,
+        links: results[3]
+      },
+      {
+        path: '/search',
+        component: SearchPage
+      },
+      {
+        path: '/search/:search',
+        component: SearchResultPage,
+        info: results[1]
+      },
+      {
+        path: '/search/:search/:info',
+        component: SearchSpotlightPage,
+        info: results[1]
+      },
+      {
+        path: '*',
+        component: RepoNotFound
+      }
+    ]
+  }
+}
+
+function renderRoutes(routes) {
+  return ReactDOM.render(
+    <ReactRouter.Router history={ReactRouter.browserHistory}>
+      {routes}
+    </ReactRouter.Router>,
+    document.querySelector('.repo-container')
+  );
+}
+
 var catPromise = helper.httpGet('https://nmsdb-55119.firebaseio.com/categories.json')
   .then(JSON.parse);
 var infoPromise = helper.httpGet('https://nmsdb-55119.firebaseio.com/info.json')
@@ -23,98 +118,9 @@ var resourcePromise = helper.httpGet('https://nmsdb-55119.firebaseio.com/resourc
 var linkPromise = helper.httpGet('https://nmsdb-55119.firebaseio.com/links.json')
   .then(JSON.parse);
 Promise.all([catPromise, infoPromise, resourcePromise, linkPromise])
-  .then((results) => {
-    // Replace category names with category objects
-    results[1].forEach((info) => {
-      info.categories.forEach((cat, index) => {
-        info.categories[index] = results[0].find((item) => {
-          return item.title === cat;
-        }) || info.categories[index];
-      });
-    });
-
-    return results;
-  })
-  .then((results) => {
-
-    var routerConf = {
-      path: '/',
-      component: RepoApp,
-      indexRoute: {
-        component: RepoHome
-      },
-      childRoutes: [
-        {
-          path: '/info',
-          component: InfoPage,
-          info: results[1]
-        },
-        {
-          path: '/info/:info',
-          component: InfoSpotlightPage,
-          info: results[1]
-        },
-        {
-          path: '/categories',
-          component: CategoryPage,
-          info: results[1],
-          categories: results[0]
-        },
-        {
-          path: '/categories/:category',
-          component: CategoryListPage,
-          info: results[1],
-          categories: results[0]
-        },
-        {
-          path: '/categories/:category/:info',
-          component: CategorySpotlightPage,
-          info: results[1],
-          categories: results[0]
-        },
-        {
-          path: '/elements',
-          component: ElementPage,
-          elements: results[2]
-        },
-        {
-          path: '/elements/:element',
-          component: ElementSpotlightPage,
-          elements: results[2]
-        },
-        {
-          path: '/links',
-          component: LinkPage,
-          links: results[3]
-        },
-        {
-          path: '/search',
-          component: SearchPage
-        },
-        {
-          path: '/search/:search',
-          component: SearchResultPage,
-          info: results[1]
-        },
-        {
-          path: '/search/:search/:info',
-          component: SearchSpotlightPage,
-          info: results[1]
-        },
-        {
-          path: '*',
-          component: RepoNotFound
-        }
-      ]
-    }
-
-    ReactDOM.render(
-      <ReactRouter.Router history={ReactRouter.browserHistory}>
-        {routerConf}
-      </ReactRouter.Router>,
-      document.querySelector('.repo-container')
-    );
-  })
+  .then(replaceCategories)
+  .then(createRouterConfig)
+  .then(renderRoutes)
   .then(() => {
     ReactRouter.browserHistory.listen((state) => {
       if (state.action === 'PUSH') {
