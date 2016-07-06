adc46269652fcec5d9845471b88e31faf845dfcf
secret_online <me+github@secretonline.co>
2016-07-07 09:47:59 +1200

    Fix line break
    Yay for jsx sysntax!

--- src/components/RepoProblem.jsx
+++ src/components/RepoProblem.jsx
@@ -7,7 +7,7 @@ export default class RepoProblem extends React.Component {
           Much of the information in the repository has come from articles and interviews that are from a lot earlier in the game's development. Due to the development process, some of this information may no longer be accurate. If you've found an inaccurate piece of information in the repository, please contact me so I can fix it.
         </p>
         <p>
-          Email: <a href="mailto://me+repo@secretonline.co">me+repo@secretonline.co</a><br>
+          Email: <a href="mailto://me+repo@secretonline.co">me+repo@secretonline.co</a><br />
           Reddit: <a href="https://www.reddit.com/message/compose/?to=secret_online">/u/secret_online</a>
         </p>
         <p>
