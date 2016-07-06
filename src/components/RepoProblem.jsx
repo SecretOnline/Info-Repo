34024ee39f45a1eaa94a97ccd46309bef7add95d
secret_online <me+github@secretonline.co>
2016-07-06 15:36:55 +1200

    Add links to contact me

--- src/components/RepoProblem.jsx
+++ src/components/RepoProblem.jsx
@@ -2,6 +2,17 @@ export default class RepoProblem extends React.Component {
   render() {
     return (
       <div>
+        <h2>Found a problem?</h2>
+        <p>
+          Much of the information in the repository has come from articles and interviews that are from a lot earlier in the game's development. Due to the development process, some of this information may no longer be accurate. If you've found an inaccurate piece of information in the repository, please contact me so I can fix it.
+        </p>
+        <p>
+          Email: <a href="mailto://me+repo@secretonline.co">me+repo@secretonline.co</a><br>
+          Reddit: <a href="https://www.reddit.com/message/compose/?to=secret_online">/u/secret_online</a>
+        </p>
+        <p>
+          Be sure to give sources for your corrections. An important part of the Information Repository is that every single bit of info has the original source available.
+        </p>
       </div>
     );
   }
