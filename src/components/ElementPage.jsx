0f9567bfb9aa0b69eec504a115bacfbd3e809ab2
secret_online <me+github@secretonline.co>
2016-07-11 15:35:19 +1200

    Change font sizes and margins

--- src/components/ElementPage.jsx
+++ src/components/ElementPage.jsx
@@ -6,7 +6,7 @@ export default class ElementPage extends React.Component {
       <div class="page element-page">
         <h2>Elements</h2>
         <ElementCardList elements={this.props.route.elements} link="/elements" />
-        <p>
+        <p className="light">
           Want all the elements on one page? Don't worry, I have <ReactRouter.Link to="/elements/all">just the link</ReactRouter.Link> for you.
         </p>
       </div>
--- src/components/InfoPage.jsx
+++ src/components/InfoPage.jsx
@@ -6,7 +6,7 @@ export default class InfoPage extends React.Component {
       <div class="page info-page">
         <h2>Information</h2>
         <InfoCardList info={this.props.route.info} link="/info" />
-        <p>
+        <p className="light">
           Want all the information on one page? Don't worry, I have <ReactRouter.Link to="/info/all">just the link</ReactRouter.Link> for you.
         </p>
       </div>
