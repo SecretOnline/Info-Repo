0fe97a2078cfd859b33b20dc56085667ae5b4306
secret_online <me+github@secretonline.co>
2016-07-22 13:00:22 +1200

    Add groups to main links page

--- src/js/pages/LinkPage.jsx
+++ src/js/pages/LinkPage.jsx
@@ -13,10 +13,36 @@ export default class LinkPage extends React.Component {
       .concat() // Create a copy of the array, don't modify original
       .sort(helper.titleSort);
 
+    // Get a list of all the groups
+    var groupList = [];
+    this.props.route.links
+      .forEach((link) => {
+        if (link.groups) {
+          link.groups.forEach((group) => {
+            if (groupList.indexOf(group) === -1) {
+              groupList.push(group);
+            }
+          });
+        }
+      });
+    var groupElements = groupList
+      .map((group) => {
+        return (
+          <li key={group}>
+            <ReactRouter.Link to={`/links/${helper.modTitle(group)}`}>
+              {group}
+            </ReactRouter.Link>
+          </li>
+        );
+      });
+
     return (
       <div class="page links-page">
         <h1>Links</h1>
-
+        <h2>Groups</h2>
+        <ul className="tag-list">
+          {groupElements}
+        </ul>
         <h2>All Links</h2>
         <LinkCardList links={allLinks} />
       </div>
