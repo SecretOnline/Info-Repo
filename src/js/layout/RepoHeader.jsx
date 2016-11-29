934d23844f0f128b16a834533f5157ab2de43960
secret_online <me+github@secretonline.co>
2016-11-30 11:12:33 +1300

    Make header's link go to info rather than landing

--- src/js/layout/RepoHeader.jsx
+++ src/js/layout/RepoHeader.jsx
@@ -16,7 +16,7 @@ export default class RepoHeader extends React.Component {
   render() {
     return (
       <header>
-        <h1 className="title" onClick={this.closeNav}><ReactRouter.Link to="/">NMS Information Repository</ReactRouter.Link></h1>
+        <h1 className="title" onClick={this.closeNav}><ReactRouter.Link to="/info">NMS Information Repository</ReactRouter.Link></h1>
         <div className="button-container">
           <button type="button" className="menu" onClick={this.toggleNav}>
             <img src="/res/menu.svg" alt="Navigation" />
